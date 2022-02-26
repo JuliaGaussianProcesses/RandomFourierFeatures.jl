47174b692ffbae969b259acaaebf3b16941517da
Ross Viljoen <ross@viljoen.co.uk>
2022-02-26 22:23:45 +0000

    add AbstractGP import

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Test
 using Random
 using RandomFourierFeatures
 using KernelFunctions
+using AbstractGPs
 
 # Writing tests:
 # 1. The file structure of the test should match precisely the file structure of src.
